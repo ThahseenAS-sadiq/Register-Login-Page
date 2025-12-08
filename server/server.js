1d7a23691b5d1d2bd83a57e747d4a5a117e17797
thahseen.sadiqas@gmail.com <thahseen.sadiqas@gmail.com>
2025-12-08 17:41:27 +0530

    add server.js and package.json

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import { connectDB } from './config/db.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -11,18 +13,30 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-
-// Connect to DB BEFORE routes
+// Connect to DB
 connectDB();
 
 // Routes
 app.use("/api/users", authRoutes);
 
-// Root route (optional)
-app.get("/", (req, res) => {
+// Serve frontend in production
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/build", "index.html"));
+  });
+} else {
+  // Root route for testing backend locally
+  app.get("/", (req, res) => {
     res.send("API is running...");
-});
+  });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
